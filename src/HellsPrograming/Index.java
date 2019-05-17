9c4f0e37815bc104e455328bfc033408c7b56c91
rosen1000 <rosen.ruseff@gmail.com>
2019-05-17 07:59:47 +0300

    implemented admin menu

--- src/HellsPrograming/Index.java
+++ src/HellsPrograming/Index.java
@@ -1,16 +1,14 @@
 package HellsPrograming;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Index extends CommandModule {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Scanner scanstr = new Scanner(System.in);
-        
         ArrayList<User> users = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
         User user = null;
-        
+
         boolean flag = true;
         while (flag) {
             int fc = num("For User registration select 1"
@@ -18,7 +16,7 @@ public class Index extends CommandModule {
                     + "\nFor Admin login select 3");
             switch (fc) {
 
-                case 1 :
+                case 1:
                     String username = read("Enter your username");
                     String nickname = read("Enter your nickname");
                     String password = read("Enter your password");
@@ -28,11 +26,11 @@ public class Index extends CommandModule {
                             users.add(new User(username, nickname, password, true));
                             say("Created as admin account, can't be deleted");
                             int size = num("how much items do you have in the shop?");
-                            ArrayList<Item> Items = new ArrayList<>();
+                            items = new ArrayList<>();
                             for (int i = 0; i < size; i++) {
-                                Items.add(new Item(read("Enter item number " + i), price("Enter price for the item"), num("And how much is in stock?")));
+                                items.add(new Item(read("Enter item number " + i), price("Enter price for the item"), num("And how much is in stock?")));
                             }
-                            
+
                         } else {
                             users.add(new User(username, nickname, password, false));
                         }
@@ -58,12 +56,12 @@ public class Index extends CommandModule {
                             break;
                         }
                     }
-                    
+
                     if (!found) {
                         say("No user found sorry :(");
                         break;
                     }
-                    
+
                     password = read("User found! Enter password");
                     if (users.get(index).checkPassword(password)) {
                         user = users.get(index);
@@ -75,7 +73,27 @@ public class Index extends CommandModule {
                     break;
             }
         }
-        
-        
+        flag = true;
+        while (flag) {
+            int choice = num("Add item"
+                    + "\n1. Delete item"
+                    + "\n2. Edit item"
+                    + "\n3. Log off");
+            switch (choice) {
+                case 1:
+                    items.add(new Item(read("Enter name for new item", price("Enter price for new item"), num("How much items do we have ion store"))));
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    user = null;
+                    flag = false;
+                    break;
+                default:
+                    say("Invalid choice");
+            }
+        }
+
     }
 }
